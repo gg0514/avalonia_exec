efe3226b280f04ba9f24172f57cc0e509ae7c1d7
cmshouse <gg0514@daum.net>
2025-01-31 01:57:28 +0900

    Approach 3 : Place a UserControl inside a DataTemplate

---
+++ 02. DataTemplates/Views/PersonView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Program.Views
+{
+    public partial class PersonView : UserControl
+    {
+        public PersonView()
+        {
+            InitializeComponent();
+        }
+    }
+}
